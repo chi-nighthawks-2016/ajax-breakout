3b4e59b87972de3d7b94eee62e50c1430f26158f
Alyssa Diaz <alydiaz77@gmail.com>
2016-06-21 13:49:44 -0500

    finished breakout

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -7,3 +7,7 @@ get '/playlists/:id' do
   content_type :json
   Playlist.find_by(id: params[:id]).tracks.to_json
 end
+
+delete '/songs/:id' do
+  Track.find(params[:id]).destroy
+end
--- public/js/application.js
+++ public/js/application.js
@@ -5,17 +5,37 @@ $(document).ready(function() {
     var url = "/playlists/" + playlistId;
 
     $.ajax({
-      url: url,
-      success: function(response) {
+      url: url
+    }).done(function(response) {
         buildSongs(response);
         $(".info").removeClass("info"); //clear other selected playlist
          $("#songsContainer").show('slow');
          $("#addSongContainer").show('slow');
          $row.addClass("info"); // indicate this playlist was selected
          $("#addSongContainer form").attr("action", url + "/songs"); //set the form to add songs to this playlist
-      }
-    })
+      })
   });
+
+  // What is my event going to be bound to?
+  // is that thing there when the page loads?  (delegated event)
+  // what event are we trapping
+  // what do we want to happen when this event occurs?
+    // ajax call
+    // what url are we hitting
+    // what method
+    // what do we do when it's done
+      // where to put result?  or what to change on the page?
+  $("#songsContainer").on("click", "i.fa-trash", function(){
+    var $row = $(this).parent().parent();
+    var songId = $row.attr("data-song-id");
+
+    $.ajax({
+      url: "/songs/" + songId,
+      method: "DELETE"
+    }).done(function(response){
+      $row.remove();
+    });
+  })
 });
 
 function buildSongs(data) {
